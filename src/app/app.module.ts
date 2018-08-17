97ea7c7f4e6fafdced0a2cb6e013e51606cf1524
reeseglasscock <reesegpdx@gmail.com>
2018-08-17 12:35:07 -0700

    add in hero main component for main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeroMainComponent } from './hero-main/hero-main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HeroMainComponent
   ],
   imports: [
     BrowserModule
